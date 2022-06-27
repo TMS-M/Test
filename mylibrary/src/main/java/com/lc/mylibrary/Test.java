86c7cbfbf65bbab3445253750457f1919169e271
孟宪毓 <18714540896@163.com>
2022-06-27 17:54:11 +0800

    first

---
+++ mylibrary/src/main/java/com/lc/mylibrary/Test.java
@@ -0,0 +1,13 @@
+package com.lc.mylibrary;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by 孟宪毓 on 2022/6/27 17:46
+ */
+public class Test {
+    public void showMessage(String message){
+        Log.d("1---1",message);
+    }
+}
